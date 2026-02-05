fab1dfa5bd72b76241432567618ea52c123a4e60
Coco Cao <cococao@Mac.fritz.box>
2026-02-05 22:06:34 +0100

    added some further clarification about definitions

--- data/constants.py
+++ data/constants.py
@@ -7,6 +7,6 @@ class Constants:
         self.current_age = int(os.environ.get("CURRENT_AGE"))
         self.retirement_age = int(os.environ.get("RETIREMENT_AGE"))
 
-        self.retirement_capital = int(os.environ.get("RETIREMENT_CAPITAL")) # capital you have at the moment of retirement, at retirement value
+        self.retirement_capital = int(os.environ.get("RETIREMENT_CAPITAL")) # capital you have at the moment of retirement, value then (at the year of retirement)
         self.current_capital = int(os.environ.get("CURRENT_CAPITAL"))
         self.average_annual_salary_increment = float(os.environ.get("AVERAGE_ANNUAL_SALARY_INCREMENT")) #since we should retire before my professional earning potential starts to dwindle
